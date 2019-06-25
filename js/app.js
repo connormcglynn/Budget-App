076e43ddeb4a40d6fbba92c9c5a8bda8151f8296
Connor McGlynn <connorappleid@intracitygeeks.org>
2019-06-25 16:12:48 -0400

    add doc defaults

---
+++ js/app.js
@@ -0,0 +1,36 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
+}
+
+eventListeners = () => {
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  // new instance of UI class
+  const UI = new UI();
+
+  // budgetForm submit
+  budgetForm.addEventListener("submit", () => {
+    eventListeners();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  eventListeners();
+});
